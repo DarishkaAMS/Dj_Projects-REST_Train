c04514209b1bcb382f9f0b546331888e7539f1f6
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-10-17 14:40:28 +0300

    Add ClassBased APIViews

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from api_basic.views import article_list, article_detail
+from api_basic.views import article_list, article_detail, ArticleAPIView, ArticleDetails
 
 urlpatterns = [
-    path('article', article_list),
-    path('article_detail/<int:pk>', article_detail),
+    # path('article', article_list),
+    path('article', ArticleAPIView.as_view()),
+    # path('article_detail/<int:pk>', article_detail),
+    path('article_detail/<int:id>', ArticleDetails.as_view()),
 ]
